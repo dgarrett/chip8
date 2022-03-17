3d064c3f96a7328e889791118d92426026306181
Dylan Garrett <dylan.garrett@gmail.com>
2022-03-16 18:49:22 -0700

    CMake building

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-c++ --std=c++17 `sdl2-config --cflags` -I lib/imgui -I lib/imgui/backends src/*.cpp lib/imgui/backends/imgui_impl_sdl.cpp lib/imgui/backends/imgui_impl_opengl3.cpp lib/imgui/imgui*.cpp `sdl2-config --libs` -framework OpenGl -framework CoreFoundation
+#!/usr/bin/env bash
+
+# c++ --std=c++17 `sdl2-config --cflags` -I lib/imgui -I lib/imgui/backends src/*.cpp lib/imgui/backends/imgui_impl_sdl.cpp lib/imgui/backends/imgui_impl_opengl3.cpp lib/imgui/imgui*.cpp `sdl2-config --libs` -framework OpenGl -framework CoreFoundation
+
+cmake -G "Unix Makefiles" -B build
+make -C build
